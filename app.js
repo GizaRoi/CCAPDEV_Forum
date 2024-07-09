291c19cd25df3dbb79055c44edd60a18404670f0
sigbinnie <aza.q.velasquez@gmail.com>
2024-07-10 00:05:48 +0800

    added bcrypt, router, login, reg

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express')
 const server = express()
 require('dotenv').config();
 
+const mongoose = require('mongoose'); //import
+
 const bodyParser = require('body-parser')
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }));
@@ -20,11 +22,13 @@ server.set('view engine', 'hbs');
 server.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
+
 server.use(bodyParser.json());
 server.set("view cache", false);
 server.use(express.static('public'));
 server.use(router);
 
+//server listen
 const port = process.env.PORT || 9090;
 server.listen(port, async function(){
     await connectToMongo();
