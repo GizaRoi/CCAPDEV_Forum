fea75ec2719a4eb739d4778b98c458a967d2fe6d
Roimarc Bilbao <roimarc_bilbao@dlsu.edu.ph>
2024-08-02 19:48:57 +0800

    a

--- src/conn.js
+++ src/conn.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const mongoURI = process.env.MONGODB_URI;
 
 function connectToMongo(dbName = process.env.DB_NAME) {
-    return mongoose.connect(mongoURI, {dbName: dbName});
+    return mongoose.connect(mongoURI, {dbName: dbName, useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true });
 };
 
 function signalHandler() {
